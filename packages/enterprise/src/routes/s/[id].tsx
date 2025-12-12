477c2b7a4a9ed7ea4d1f39f2924caa00f6ab6c78
Kyle Crommett <kyle@latitudes.io>
2025-12-11 21:56:59 -0800

    fix: convert short URL redirect to API route for proper 301 status

--- packages/enterprise/src/routes/s/[id].tsx
+++
@@ -1,8 +0,0 @@
-import { redirect } from "@solidjs/router"
-import { useParams } from "@solidjs/router"
-
-export default function ShortRedirect() {
-  const params = useParams()
-  // Redirect /s/{id} to /share/{id}
-  throw redirect(`/share/${params.id}`, 301)
-}
